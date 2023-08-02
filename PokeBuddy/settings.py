7e6e66dc6ec7047f4410d68664fbd0ee86d298b7
Jonathon Chenvert <jchenvert1@gmail.com>
2023-08-02 07:38:06 -0700

    Revive project to a working(ish) state

--- PokeBuddy/settings.py
+++ PokeBuddy/settings.py
@@ -19,8 +19,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-with open(BASE_DIR + '/PokeBuddy/secret_key.txt') as f:
+# SECURITY WARNING: keep the secret key used in production a secret!
+with open(f'{BASE_DIR}/PokeBuddy/secret_key') as f:
     SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'PokeBuddy.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': "postgres",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "db", # set in docker-compose.yml
+        "PORT": 5432, # default PostgreSQL port
     }
 }
 
--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+# Getting Started
+
+Run the `util/generate_secret_key.py` file to generate a secret key that Django can use.
+
 # Disclaimers
 
 Pokémon © 2002-2019 Pokémon. © 1995-2019 Nintendo/Creatures Inc./GAME FREAK inc. TM, ® and Pokémon character names are trademarks of Nintendo.
